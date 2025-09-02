18fd4e9579dd75e7bb11cf54cbd84d336f5ee3c0
ThammisettyMahi0312 <thammisettybannymahi@gmail.com>
2025-09-02 16:00:56 +0530

    Updates

--- backend/Inventory.API/Inventory.API/obj/Debug/net8.0/Inventory.API.AssemblyInfo.cs
+++ backend/Inventory.API/Inventory.API/obj/Debug/net8.0/Inventory.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2823df79403e11b4b6b39581046ce6f48b81389b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
